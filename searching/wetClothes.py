e6bd5bafdc1c5f9822f4f1ded031e6995660e601
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-12-19 23:01:15 +0530

    update

--- searching/wetClothes.py
+++ searching/wetClothes.py
@@ -21,38 +21,21 @@ Print the maximum number of clothes we can collect.
 '''
 
 
-def linsearch(nums, t, s):
-    n = len(nums)
-    cnt = 0
-    if s >= n:
-        return 0
-    for i in range(s,n):
-        if nums[i] <= t:
-            cnt += 1
-        else:
-            break
-    return cnt
-
 def solve():
-    n, m, g = map(int, input().split())
-    rain = list(map(int, input().split()))
-    drytime = list(map(int, input().split()))
+    n,m,g = input().split()
+    t = list(map(int,input().split()))
+    dt = list(map(int,input().split()))
 
-    durs = []
-    last = 0
-    for t in rain:
-        durs.append(t-last)
-        last = t
-    durs.sort(reverse=True)
-    drytime.sort()
-    ans = 0
-    s = 0
-    for i in range(g):
-        if i >= len(durs):
-            break
-        cnt = linsearch(drytime, durs[i], s)
-        s += cnt
-        ans += cnt
-    print(ans)
+    gap =[]
+    for i in range(len(t)-1):
+        x = t[i+1]-t[i]
+        gap.append(x)
+        
+    m = max(gap)
+    c=0
+    for i in range(len(dt)):
+        if dt[i]<=m:
+            c+=1
+    print(c)
     
 solve()
