cfb5a5db29b41fce49e1a2c6df05b9536a183494
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-12-19 22:46:25 +0530

    update

--- searching/MonkandSpecialInteger.py
+++ searching/MonkandSpecialInteger.py
@@ -1,3 +1,28 @@
+'''
+Question:
+
+Monk and his friend Micro found an injured array A of N integers. 
+The array was missing its special integer K, 
+which is an integer such that no subarray of size K has a sum greater than X.
+
+Monk wants to gift the array the maximum possible value of K to calm it down. 
+Micro needs your help to find this value.
+
+Input Format:
+
+First line: Two space-separated integers N and X.
+Second line: N space-separated integers, the elements of the array A.
+Output Format:
+
+Print the maximum possible value of the special integer K.
+
+Constraints:
+
+1 ≤ N ≤ 10^5
+1 ≤ X ≤ 10^18
+1 ≤ A[i] < min(X, 10^9)
+'''
+
 n,x = map(int, input().split())
 A = list(map(int, input().split()))
 
